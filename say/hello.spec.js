80ce2ed7399a7fd362d9a89b1b1a7a2fc691bf45
Karl <karl.turrell@buto.tv>
2017-02-26 18:56:20 +0000

    Add unit test for sayHello in body paragraph

--- say/hello.spec.js
+++ say/hello.spec.js
@@ -1,15 +1,23 @@
-import { sayHello } from './hello'
+import { sayHello, sayHelloConsole } from './hello'
 
 const chai = require('chai')
 const sinon = require('sinon')
 let should = chai.should()
 
+const d3 = require('d3')
+
 describe('Say Module', function() {
-    it('should console log Hello World', function() {
+    it('should console log Hello world', function() {
         let consoleLogSpy = sinon.spy(console, 'log')
 
-        sayHello()
+        sayHelloConsole()
 
         consoleLogSpy.withArgs('Hello world').calledOnce.should.be.true
+    })
+
+    it('should append a p tag containing Hello world to the body', function() {
+        sayHello()
+
+        d3.select('body').selectAll('p').size().should.equal(1)
     })
 })
